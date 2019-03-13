4a6fc21d8df58310eaca5a3454dac67aa7463637
Seulki Bae <kinchi22@gmail.com>
2019-03-13 17:36:16 +0900

    Add AboutMe component

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="aboutme">
+    <h2>Hello everyone, my name is {{ name }}.</h2>
+    <h3>I'm {{ age }}</h3>
+    <p>{{ phrase }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class AboutMe extends Vue {
+  @Prop() private name!: string;
+  @Prop() private age!: number;
+  @Prop() private phrase!: string;
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <AboutMe :name="myName" :age="myAge" :phrase="myPhrase"/>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import AboutMe from '@/components/AboutMe.vue';
+
+@Component({
+  components: {
+    AboutMe,
+  },
+})
+export default class About extends Vue {
+  private myName: string = 'Seulki Bae';
+  private myAge: number = 3;
+  private myPhrase: string = 'I\'m hungry';
+}
+</script>
